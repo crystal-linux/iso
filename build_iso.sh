244118ca817d440a4e59055c0803ac9caccd3990
Matt Compton <matt@xhec.us>
2021-04-18 14:39:30 -0400

    tired

--- build_iso.sh
+++ build_iso.sh
@@ -3,10 +3,10 @@ rm -fv crystal-live-*.iso
 
 WORKDIR=$(mktemp -d)
 # idk if this would've happened automatically?
-cp pear/pacman.conf pear/airootfs/etc/.
-cp pear/packages.x86_64 pear/airootfs/etc/packages.x86_64
-cp /etc/pacman.d/mirrorlist pear/airootfs/etc/pacman.d/.
-echo "Built on $(date +"%D @ %T EST")" > pear/airootfs/etc/buildstamp
+cp crystal/pacman.conf crystal/airootfs/etc/.
+cp crystal/packages.x86_64 crystal/airootfs/etc/packages.x86_64
+cp /etc/pacman.d/mirrorlist crystal/airootfs/etc/pacman.d/.
+echo "Built on $(date +"%D @ %T EST")" > crystal/airootfs/etc/buildstamp
 time sudo ./mkarchiso -v -w $WORKDIR -o . crystal
 sudo rm -rf $WORKDIR
 
