430a173fb181c69ce9b2d21ccb7b5a5540151b5b
Matt Compton <matt@xhec.us>
2021-04-20 14:36:00 -0400

    add cli prof for testing

--- build_iso.sh
+++ build_iso.sh
@@ -1,15 +1,17 @@
-rm -fv crystal-live-*.iso
+rm -fv *.iso
 
+if [[ "$1" == "" ]]; then
+    printf "Profile: "
+    read prof
+else
+    prof=$1
+fi
 
 WORKDIR=$(mktemp -d)
 # idk if this would've happened automatically?
-cp crystal/pacman.conf crystal/airootfs/etc/.
-cp crystal/packages.x86_64 crystal/airootfs/etc/packages.x86_64
-cp /etc/pacman.d/mirrorlist crystal/airootfs/etc/pacman.d/.
-echo "Built on $(date +"%D @ %T EST")" > crystal/airootfs/etc/buildstamp
-time sudo ./mkarchiso -v -w $WORKDIR -o . crystal
+cp ${prof}/pacman.conf ${prof}/airootfs/etc/.
+cp ${prof}/packages.x86_64 ${prof}/airootfs/etc/packages.x86_64
+cp /etc/pacman.d/mirrorlist ${prof}/airootfs/etc/pacman.d/.
+echo "Built on $(date +"%D @ %T EST")" > ${prof}/airootfs/etc/buildstamp
+time sudo ./mkarchiso -v -w $WORKDIR -o . ${prof}
 sudo rm -rf $WORKDIR
-
-if [[ "$1" == "docker" ]]; then
-    cp *.iso /output/.
-fi
