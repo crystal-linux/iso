20f61799f772cf2d746dc6ff0c7bb0a21ab9c731
Matt C <mcompton2002@gmail.com>
2021-05-07 13:39:34 -0400

    fucked that up lmao

--- chrooted.sh
+++ chrooted.sh
@@ -19,7 +19,7 @@ chmod +x /usr/bin/mirrorsetup
 echo "menu" >> /home/crystal/.bashrc
 chmod +x /etc/crystal/bootopts/*
 systemctl enable gdm
-sed -i 's/#en_US.UTF-8 UTF-8/#en_US.UTF-8 UTF-8/g' /etc/locale-gen
+sed -i 's/#en_US.UTF\-8 UTF\-8/#en_US.UTF\-8 UTF\-8/g' /etc/locale-gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "Configured the system user. Exiting chroot."
