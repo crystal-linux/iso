2fea16628fad0fa4e3b59bae68691dbadeff9bc8
Matt C <mcompton2002@gmail.com>
2021-05-11 18:38:30 -0400

    upload script

---
+++ upload_iso.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ "$1" == "" ]]; then
+    printf "Filename: "
+    read FN
+else
+    FN="$1"
+fi
+
+scp $FN crystalux@salyut.one:public_html/isos/.
+ssh crystalux@salyut.one "ln -sf public_html/isos/$FN public_html/isos/latest"
