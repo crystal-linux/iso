ace52b4f6b0fe6deec2a48866f7d4407f35570ad
Matt C <mcompton2002@gmail.com>
2021-05-14 13:04:40 -0400

    upload releases to GH

--- upload_iso.sh
+++ upload_iso.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-if [[ "$1" == "" ]]; then
-    printf "Filename: "
-    read FN
-else
-    FN="$1"
-fi
+printf "Ver: "
+read VER
 
-scp $FN crystalux@salyut.one:public_html/isos/.
-ssh crystalux@salyut.one "rm public_html/isos/crystal-latest.iso && ln -s public_html/isos/$FN public_html/isos/crystal-latest.iso"
+gh release create $VER
+gh release upload $VER *.iso
