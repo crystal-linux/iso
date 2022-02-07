16c16d36d14149595af5386935781f4f65b6d4d3
Matt Compton <matt@tar.black>
2022-02-07 01:42:39 +0000

    Update profiledef.sh

--- crystal/profiledef.sh
+++ crystal/profiledef.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="crystal-live"
+iso_name="agamOS-crystal-live"
 iso_label="CRYS_$(date +%Y%m)"
 iso_publisher="Crystal Linux <https://getcryst.al>"
 iso_application="Crystal Linux Live"
