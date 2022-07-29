42e2204530bfa4cf1c0df6f62c19b758e86f22ac
Michal <michal@tar.black>
2022-07-29 23:54:53 +0100

    HUGE Iso cleanup

--- build_iso.sh
+++ build_iso.sh
@@ -10,15 +10,12 @@ else
     fi
 fi
 
-cp pacman.def crystal/pacman.conf
-cp crystal/pacman.conf crystal/airootfs/etc/.
-
-cp crystal/packages.x86_64 crystal/airootfs/etc/packages.x86_64
-cp /etc/pacman.d/mirrorlist crystal/airootfs/etc/pacman.d/.
+# Copy pacman config to the airootfs
+cp crystal/pacman.conf crystal/.
+cp crystal/packages.x86_64 crystal/.
 
 echo "Built on $(date +"%D @ %T EST")" > crystal/airootfs/etc/buildstamp
 
-cp chrooted-iso.sh chrooted.sh
 time sudo ./mkarchiso -v -w $WORKDIR -o . crystal
 
 sudo rm -rf $WORKDIR
