b1bfa243bc4b7f9b3a12ff984695b24b19eb2e5b
Michal S <michal@tar.black>
2022-08-19 16:36:26 +0100

    fixed mirrorlist issue adn cleaned up bootstrap

--- chrooted-bootstrap.sh
+++ chrooted-bootstrap.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 echo "Chrooted in the new system, running as $(whoami)"
 
-# Services
-systemctl enable NetworkManager
-systemctl enable mirrorlist
-systemctl enable gdm
+# Mirrorlist
+reflector > /etc/pacman.d/mirrorlist
 
-# Hostname and Locale
+# Hostname
 echo "crystal-live" > /etc/hostname
+
+# Locale
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
+locale-gen
+
+
 echo "Configured the system. Exiting chroot."
