153afb28660a332b4d1c06bad911d24cb50c89eb
Matt C <mdc028@bucknell.edu>
2022-09-29 02:08:32 +0000

    Closes [FEATURE]: Request ISO-8601 date format be standardized on #21

--- crystal/profiledef.sh
+++ crystal/profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="crystal-live"
-iso_label="CRYS_$(date +%Y%m)"
+iso_label="CRYS_$(date +'%Y-%m-%d')"
 iso_publisher="Crystal Linux <https://getcryst.al>"
 iso_application="Crystal Linux Live"
-iso_version="$(date +"%m-%d-%g-%H-%M")"
+iso_version="$(date +'%Y-%m-%d')"
 install_dir="arch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
